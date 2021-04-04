21c114b917d0ff239526aad5687add667f805f26
MikeMike39 <Mfonseca294@gmail.com>
2021-04-04 10:39:34 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// function writeCards( namesArray, event ) {
+//     let thankYouCards = []
+//     for ( let i = 0; i < namesArray.length; i++ ) {
+//       thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+//     }
+//     return thankYouCards
+//   }
+
+//   function countDown( startingNumber ) {
+//     while ( startingNumber > 0 ) {
+//       console.log( startingNumber );
+//       startingNumber -= 1;
+//     }
+//     console.log( startingNumber );
+//   }
+
+// Creates function "writeCards" returns an "array[]" of thank you messages for each name provided to the function
+function writeCards(tricks, treats) {
+  let thankS = [];
+  for (let i = 0; i < tricks.length; i++) {
+    thankS[i] = `Thank you, ${tricks[i]}, for the wonderful ${treats} gift!`;
+  }
+  return thankS;
+}
+
+// Logs each number when counting down, starting from the number provided ("-1" = "0" starting point)
+function countDown(fromNum) {
+  for (let i = fromNum; i > -1; i--) {
+    console.log(i); //Invokes console.log once for each number, counting down from the number provided to zero
+  }
+}
